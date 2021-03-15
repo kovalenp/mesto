4a78fdd2209073f965f5a99a57cc1f41bc8a3f31
Pavel Kovalenko <pavel@kovalenko.cz>
2021-03-15 13:51:14 +0100

    feature: added form and submit events

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,33 @@
 const editButton = document.querySelector('.profile__edit');
 const closeButton = document.querySelector('.popup__close');
 
-editButton.addEventListener('click', function () {
-  document.querySelector('.popup').classList.add('popup_opened');
-});
+const formElement = document.querySelector('.popup__form');
 
-closeButton.addEventListener('click', function () {
+const profileName = document.querySelector('.profile__name');
+const profileRole = document.querySelector('.profile__role');
+
+const usernameInput = document.querySelector('#username');
+const roleInput = document.querySelector('#role');
+
+editButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+function openPopup() {
+  document.querySelector('.popup').classList.add('popup_opened');
+  usernameInput.value = profileName.innerText || '';
+  roleInput.value = profileRole.innerText || '';
+}
+
+function closePopup() {
   document.querySelector('.popup').classList.remove('popup_opened');
-});
+}
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+  profileName.textContent = usernameInput.value;
+  profileRole.textContent = roleInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
