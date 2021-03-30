79bc10f988f8720349c600049f5ce0903c9a923e
Pavel Kovalenko <pavel@kovalenko.cz>
2021-03-30 16:04:43 +0200

    Added init state, like and remove

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,39 @@
+const initData = [
+  {
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
 const editButton = document.querySelector(".profile__edit");
 const closeButton = document.querySelector(".popup__close");
+
 const likeButtons = document.querySelectorAll(".places__like");
 
 const formElement = document.querySelector(".popup__form");
@@ -11,6 +45,12 @@ const profileRole = document.querySelector(".profile__role");
 
 const usernameInput = document.querySelector("#username");
 const roleInput = document.querySelector("#role");
+
+const cardTemplate = document
+  .querySelector(".places__item-template")
+  .content.querySelector(".places__item");
+
+const placesList = document.querySelector(".places__list");
 
 editButton.addEventListener("click", openPopup);
 
@@ -33,10 +73,29 @@ function formSubmitHandler(e) {
   closePopup();
 }
 
+function onClickLikeButtonHandler(e) {
+  e.target.classList.toggle("places__like_active");
+}
+
 formElement.addEventListener("submit", formSubmitHandler);
 
-likeButtons.forEach((likeButton) => {
-  likeButton.addEventListener("click", function () {
-    likeButton.classList.toggle("places__like_active");
+function addCard(card) {
+  const cardItem = cardTemplate.cloneNode(true);
+  const cardImg = cardItem.querySelector(".places__img");
+  const cardName = cardItem.querySelector(".places__name");
+  const cardDelete = cardItem.querySelector(".places__delete");
+  const cardLike = cardItem.querySelector(".places__like");
+
+  cardImg.src = card.link;
+  cardName.textContent = card.name;
+
+  cardDelete.addEventListener("click", () => {
+    cardItem.remove();
   });
-});
+
+  cardLike.addEventListener("click", onClickLikeButtonHandler);
+
+  placesList.append(cardItem);
+}
+
+initData.forEach(addCard);
