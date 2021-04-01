3ff83d22130ff21f49a23ad43e6cc10d3532b087
Pavel Kovalenko <pavel@kovalenko.cz>
2021-04-01 09:26:19 +0200

    Comments addressed

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,119 @@
+const initData = [
+  {
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
 const cardElementTemplate = document
-  .querySelector(".places__item-template")
+  .querySelector("#place-template")
   .content.querySelector(".places__item");
 
 const placesList = document.querySelector(".places__list");
 
+const addCardButton = document.querySelector(".profile__add");
+const modalAdd = document.querySelector(".modal_add");
+const modalAddCloseButton = document.querySelector(".modal__close_add");
+const addForm = document.querySelector(".modal__form_add");
+const nameInput = document.querySelector("#name");
+const linkInput = document.querySelector("#link");
+
+const modalPhoto = document.querySelector(".modal_photo");
+
+const modalPhotoImg = modalPhoto.querySelector(".photo__img");
+const modalPhotoCaption = modalPhoto.querySelector(".photo__caption");
+const modalPhotoCloseButton = modalPhoto.querySelector(".modal__close_photo");
+
+const profileEditButton = document.querySelector(".profile__edit");
+const profileCloseButton = document.querySelector(".modal__close_profile");
+
+const profileForm = document.querySelector(".modal__form_profile");
+const profileModal = document.querySelector(".modal_profile");
+const profileName = document.querySelector(".profile__name");
+const profileRole = document.querySelector(".profile__role");
+const usernameInput = document.querySelector("#username");
+const roleInput = document.querySelector("#role");
+
+addCardButton.addEventListener("click", openAddModal);
+addForm.addEventListener("submit", addFormSubmitHandler);
+modalPhotoCloseButton.addEventListener("click", () => closeModal(modalPhoto));
+profileEditButton.addEventListener("click", openProfileModal);
+profileCloseButton.addEventListener("click", () => closeModal(profileModal));
+profileForm.addEventListener("submit", profileSubmitHandler);
+
 function closeModal(modal) {
   modal.classList.remove("modal_opened");
+}
+
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
+
+function openPhotoModal(card) {
+  modalPhotoImg.src = card.link;
+  modalPhotoImg.alt = `Большая и очень красивая фотография ${card.name}`;
+  modalPhotoCaption.textContent = card.name;
+  openModal(modalPhoto);
+}
+
+function clearAddFormInputs() {
+  nameInput.value = "";
+  linkInput.value = "";
+}
+
+function addFormSubmitHandler(e) {
+  e.preventDefault();
+  const card = { name: nameInput.value, link: linkInput.value };
+  addPlaces(card, true);
+  closeModal(modalAdd);
+  clearAddFormInputs();
+}
+
+function openAddModal() {
+  openModal(modalAdd);
+  modalAddCloseButton.addEventListener("click", () => {
+    closeModal(modalAdd);
+    clearAddFormInputs();
+  });
+}
+
+function openProfileModal() {
+  openModal(profileModal);
+  usernameInput.value = profileName.textContent;
+  roleInput.value = profileRole.textContent;
+}
+
+function profileSubmitHandler(e) {
+  e.preventDefault();
+  profileName.textContent = usernameInput.value;
+  profileRole.textContent = roleInput.value;
+  closeModal(profileModal);
 }
 
 function onClickLikeButtonHandler(e) {
@@ -35,3 +143,5 @@ function addPlaces(card, isFirstElement = false) {
   const cardItem = createCardHtmlElement(card);
   isFirstElement ? placesList.prepend(cardItem) : placesList.append(cardItem);
 }
+
+initData.forEach(addPlaces);
